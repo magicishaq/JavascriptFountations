5ec96f4583a5ea1aec7cd7ff70d8750d393e3963
Ishaq Khan <ikhan@uk.nex.global>
2020-12-05 18:45:55 +0000

    finished function expressions

--- deep-js-foundations-v2-exercises/scope-exercises/function-expressions/ex.js
+++ deep-js-foundations-v2-exercises/scope-exercises/function-expressions/ex.js
@@ -1,13 +1,41 @@
 function printRecords(recordIds) {
-	// TODO
+	// TODOa
+	var theStudents = studentRecords.filter(function getIds (record) {
+	    return recordIds.includes(record.id)
+	})
+	theStudents = theStudents.sort(function sortByName (a, b) {
+		return a.name > b.name ? 1 : -1
+	})
+
+    theStudents.forEach(function printToConsole(record) {
+        console.log(`${record.name} ${record.id} :  ${record.paid? 'Paid' : 'Not Paid'}`); 
+    })
+// 	return theStudents; 
 }
 
 function paidStudentsToEnroll() {
-	// TODO
+var arr = [...studentRecords];
+var currentE = [...currentEnrollment]; 
+var paidButNotEnrolled = arr.filter(function getPaidButNotEnrolled (record) {
+    return record.paid == true && !currentE.includes(record.id)
+})
+
+var paidIds = paidButNotEnrolled.reduce(function getTheIds (ids, object){
+    return ids.concat(object.id) 
+}, [])
+return [...paidIds,...currentE]; 
+
 }
 
 function remindUnpaid(recordIds) {
-	// TODO
+    var arr = [...studentRecords]
+    var unpaid = arr.filter(function getUpaid (record) {
+        return !record.paid && recordIds.includes(record.id) 
+    })
+    var unpaidIDs = unpaid.reduce(function getUnpaidId(current, object) {
+        return current.concat(object.id)
+    }, [])
+    printRecords(unpaidIDs); 
 }
 
 
